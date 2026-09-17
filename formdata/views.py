b0c9241a5c5d5b14d75d44a6debdd81b14099ae2
Thomas Dion <tdion@bu.edu>
2026-09-17 12:04:15 -0400

    initilize context properly

--- formdata/views.py
+++ formdata/views.py
@@ -4,32 +4,27 @@ from django.shortcuts import render
 
 def show_form(request):
     '''Show the web page with the form.'''
- 
- 
     template_name = "formdata/form.html"
     return render(request, template_name)
 
 def submit(request):
     '''Process the form submission, and generate a result.'''
  
- 
+    context = {}
     template_name = "formdata/confirmation.html"
  
  
     # read the form data into python variables:
     if request.POST:
- 
- 
         name = request.POST['name']
         favorite_color = request.POST['favorite_color']
  
  
         context = {
             'name': name,
-            'favorite_color':  favorite_color,
-            
+            'favorite_color': favorite_color,      
         }
  
  
-    return render(request, template_name, context=context)
+    return render(request, template_name=template_name, context=context)
 
