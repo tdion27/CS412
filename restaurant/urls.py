fd154e7261b077b99753489ac8272e63a74f4856
Thomas Dion <tdion@bu.edu>
2026-09-23 16:13:19 -0400

    added comments and special instructions

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,10 +1,14 @@
+# File: urls.py
+# Author: Thomas Dion (tdion@bu.edu), 9/17/2026
+# Description: Contains the url paths for the restaurant app
+
 from django.urls import path
 from django.conf import settings
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
  
- 
+# define the url patterns for the application
 urlpatterns = [ 
     path(r'', views.main, name="main"),          
     path('order/', views.order, name="order"),
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,10 +1,17 @@
+# File: urls.py
+# Author: Thomas Dion (tdion@bu.edu), 9/17/2026
+# Description: Contains the views for the restaurant app
+
 from django.shortcuts import render
 import random
 import time
 
 # Create your views here.
+
+# Link to the image used for the restaurant
 image = "https://images.squarespace-cdn.com/content/v1/56d8740586db43f34bc3e823/1466713071671-UB340J4VH2UW48VDMPHU/appgrill_atetrack001.jpg"
 
+# List of specials
 specials = [
     "Wild Mushroom Flatbread",
     "Bourbon-Glazed Pork Chop",
@@ -16,7 +23,9 @@ specials = [
     "Catfish Po'Boy",
     "Braised Short Ribs",
 ]
+
 def main(request):
+    '''The homepage of the restaurant'''
     template = "restaurant/main.html"
     context = {
         'image': image,
@@ -25,23 +34,31 @@ def main(request):
 
 
 def order(request):
+    '''Allow users to create an order'''
     template = "restaurant/order.html"
     context = {
+        # Choose a random special for the menu
         'special': random.choice(specials),
     }
     return render(request, template, context)
 
 def submit(request):
-    '''Process the form submission, and generate a result.'''
- 
+    '''Process the order submission, and generate a receipt and total of selected items.'''
+
+    #Set an empty context to avoid passing in null
     context = {}
     template_name = "restaurant/confirmation.html"
     if request.POST:
-        name = request.POST['name'] 
+        #Set customer name from the form
+        name = request.POST['name']
+        #Set special instructions from the form
+        instructions = request.POST['instructions']
 
+        #Initialize variables for list of items and total
         total = 0
         orderItems = []
 
+        #Check for the presence of each item and append it to the list and total, if found in the order
         if 'chicken' in request.POST:
             orderItems.append('Mountain Heritage Fried Chicken')
             total += 15
@@ -70,6 +87,7 @@ def submit(request):
             orderItems.append('Special')
             total += 15
 
+        # Pick a random time for the order, add it to the current time, and then format it to be displayed
         random_time = random.uniform(1800, 3600)
         pickup_time = time.time() + random_time
         pickup_time_seconds = time.ctime(pickup_time)
@@ -79,6 +97,7 @@ def submit(request):
             'order': orderItems,
             'total': total,
             'pickupTime': pickup_time_seconds,
+            'instructions': instructions,
         }
 
     
